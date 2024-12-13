dca5628ec175f6ab9d4c86c89920a9f056c4b930
Juline Adam <juline.adam@edu.univ-fcomte.fr>
2024-12-13 14:18:29 +0100

    lightbox

--- src/js/script.js
+++ src/js/script.js
@@ -31,3 +31,25 @@ if (carousel && prevButton && nextButton && premierItem) {
     });
 }
 
+/* lightbox */
+const lightbox = document.querySelector("#lightbox");
+const lightboxImg = document.querySelector("#lightbox > img");
+
+document.body.addEventListener("click", (evt) => {
+    if (evt.target.matches(".schedule__film-img")) {
+        lightboxImg.src = evt.target.src;
+        lightbox.classList.add("entrée");
+        lightbox.showModal();
+    }
+});
+
+lightbox.addEventListener("click", (evt) => {
+    if (evt.target === lightbox) {
+        lightbox.classList.add("sortie");
+        lightbox.addEventListener("animationend", () => {
+            lightbox.classList.remove("sortie", "entrée");
+            lightbox.close();
+        }, { once: true });
+    }
+});
+
