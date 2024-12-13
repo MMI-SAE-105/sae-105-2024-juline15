2bdbff2d0d797426ce306d75926482154ebaad0e
Juline Adam <juline.adam@edu.univ-fcomte.fr>
2024-12-13 09:30:12 +0100

    programmation, js, readme

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,4 @@
+/* menu */
 const toggle = document.querySelector(".menu-btn");
 const nav = document.querySelector(".menu");
 const page = document.body;
@@ -12,3 +13,20 @@ if (toggle && nav) {
     });
 }
 
+/* Carousel */
+const carousel = document.querySelector(".film__gallery");
+const prevButton = document.querySelector(".carousel__button--prev");
+const nextButton = document.querySelector(".carousel__button--next");
+const premierItem = document.querySelector(".films__poster");
+
+if (carousel) {
+    prevButton.addEventListener("click", () => {
+        const scrollAmount = premierItem.clientWidth;
+        carousel.scrollBy({ left: -scrollAmount, behavior: "smooth" });;
+    });
+
+    nextButton.addEventListener("click", () => {
+        const scrollAmount = premierItem.clientWidth;
+        carousel.scrollBy({ left: +scrollAmount, behavior: "smooth" });;
+    });
+}
